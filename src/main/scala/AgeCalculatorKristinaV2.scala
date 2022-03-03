bef26ce16944df4705bb01c6cad282572a08e49d
ristinak <ristinak@gmail.com>
2022-03-03 21:24:08 +0200

    Defined val smiley as a Char instead of String.

--- src/main/scala/AgeCalculatorKristinaV2.scala
+++ src/main/scala/AgeCalculatorKristinaV2.scala
@@ -7,7 +7,7 @@ object AgeCalculatorKristinaV2 extends App {
   val name = readLine("What is you name?\n")
   val age = readLine(s"Hello, $name! How old are you?\n")
   var centenary = currentYear + targetAge - age.toInt
-  val smiley = "\u263a"
+  val smiley = '\u263a'
   val yesORno = readLine("Have you celebrated your birthday this year? Type y or n\n")
     if(yesORno == "y"){
     println(s"Great news! You will be $targetAge years old in $centenary! $smiley")
