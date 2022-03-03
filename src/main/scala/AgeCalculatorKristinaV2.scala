68ddb67e1858eab36ab154a18b91a35789c55a4b
ristinak <ristinak@gmail.com>
2022-03-03 21:22:25 +0200

    Fixed spelling and got rid of the "magic" number by adding val targetAge

--- src/main/scala/AgeCalculatorKristinaV2.scala
+++ src/main/scala/AgeCalculatorKristinaV2.scala
@@ -3,16 +3,17 @@ import scala.io.StdIn.readLine
 
 object AgeCalculatorKristinaV2 extends App {
   val currentYear = Year.now.getValue
+  val targetAge = 100
   val name = readLine("What is you name?\n")
   val age = readLine(s"Hello, $name! How old are you?\n")
-  var centeniary = currentYear + 100 - age.toInt
+  var centenary = currentYear + targetAge - age.toInt
   val smiley = "\u263a"
   val yesORno = readLine("Have you celebrated your birthday this year? Type y or n\n")
     if(yesORno == "y"){
-    println(s"Great news! You will be 100 years old in $centeniary! $smiley")
+    println(s"Great news! You will be $targetAge years old in $centenary! $smiley")
   } else if(yesORno == "n"){
-      val newCenteniary = centeniary - 1
-      println(s"Great news! You will be 100 years old in $newCenteniary! $smiley")
+      val newCentenary = centenary - 1
+      println(s"Great news! You will be $targetAge years old in $newCentenary! $smiley")
   } else{
     println("Sorry, don't understand that.")
   }
