517124369f5a198364dd50ffb3d6ba2c3909060b
ristinak <ristinak@gmail.com>
2022-03-03 20:21:07 +0200

    Initial commit

---
+++ src/main/scala/Day3AgeCalculatorKristina.scala
@@ -0,0 +1,11 @@
+import java.time.Year
+import scala.io.StdIn.readLine
+
+object Day3AgeCalculatorKristina extends App {
+  val currentYear = Year.now.getValue
+  val name = readLine("What is you name?\n")
+  val age = readLine(s"Hello, $name! How old are you?\n")
+  val centeniary = currentYear + 100 - age.toInt
+  val smiley = "\u263a"
+  println(s"Great news! You will be 100 years old in $centeniary! $smiley")
+}
