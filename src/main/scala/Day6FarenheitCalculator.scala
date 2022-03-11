67b9a609d6b3470c56ffdb359cf77558504001ad
ristinak <ristinak@gmail.com>
2022-03-11 17:40:39 +0200

    Fixed precision to 2 points after comma.

--- src/main/scala/Day6FarenheitCalculator.scala
+++ src/main/scala/Day6FarenheitCalculator.scala
@@ -1,10 +1,11 @@
 object Day6FarenheitCalculator extends App {
 
   def celciusToFarenheit(tempInCelcius: Double = 36.6): Double = {
-    (32 + tempInCelcius*9/5)
+    32 + (tempInCelcius*180.0).round/100.0
   }
 
   println(s"36.6 degrees C is ${celciusToFarenheit()} degrees Farenheit.")
   println(s"37 degrees C is ${celciusToFarenheit(37)} degrees Farenheit.")
   println(s"48.5 degrees C is ${celciusToFarenheit(48.5)} degrees Farenheit.")
+
 }
