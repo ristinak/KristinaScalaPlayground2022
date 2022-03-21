8a95c9f559d2ec2615e4e87057ebc2429663c951
ristinak <ristinak@gmail.com>
2022-03-21 20:23:32 +0200

    Fixed printing of Sequences and included negative odd numbers

--- src/main/scala/Day8CubeSequence.scala
+++ src/main/scala/Day8CubeSequence.scala
@@ -13,10 +13,11 @@ object Day8CubeSequence extends App {
   val endNumber = readLine("Enter ending number: ").toInt
 
   val cubeSequence = for (number <- startNumber to endNumber) yield Math.pow(number, 3).toInt
+  val cubeSequenceString = cubeSequence.mkString(", ")
+  println(s"The cubes of all numbers between $startNumber and $endNumber are: $cubeSequenceString")
 
-  println(s"The cubes of all numbers between $startNumber and $endNumber are: $cubeSequence")
-
-  val oddCubes = for (cube <- cubeSequence if cube % 3 == 0) yield cube
-  println(s"And the odd cubes are $oddCubes")
+  val oddCubes = for (cube <- cubeSequence if cube % 2 != 0) yield cube
+  val oddCubesString = oddCubes.mkString(", ")
+  println(s"Among them, the odd cubes: $oddCubesString")
 
 }
