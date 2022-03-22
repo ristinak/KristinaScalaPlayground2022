3d053f7d7b1a0614576fd30dc56acfd238279b08
ristinak <ristinak@gmail.com>
2022-03-22 20:42:29 +0200

    Added a comment to indicate a flaw.

--- src/main/scala/Day9WordLengthExercise.scala
+++ src/main/scala/Day9WordLengthExercise.scala
@@ -11,6 +11,7 @@ object Day9WordLengthExercise extends App {
 
   val sentence = readLine("Please enter a sentence: ").trim
   val betterSentence = sentence.replaceAll("\\p{Punct}","") // from Java, found online
+  // it only works if there are spaces between words, not only punctuation
   val words = betterSentence.split(" ")
   val wordLengths = words.map(_.length)
   val wordsOverFive = words.filter(_.length > 5)
