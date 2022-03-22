9d686efa174969badb24ae81898029376ff01f76
ristinak <ristinak@gmail.com>
2022-03-22 20:33:43 +0200

    Deleted the unnecessary .toSeq

---
+++ src/main/scala/Day9WordLengthExercise.scala
@@ -0,0 +1,20 @@
+import scala.io.StdIn.readLine
+//Ask user to enter a sentence
+//Split sentence into words using split, you will will have a sequence of words, we did this on Day 8
+//Generate word length sequence (can use map or yield)
+//Filter only words of length over 5
+//print word lengths for every word
+//print the long words
+
+//you are allowed to use yield or map/filter
+object Day9WordLengthExercise extends App {
+
+  val sentence = readLine("Please enter a sentence: ")
+  val words = sentence.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)")
+  val wordLengths = words.map(_.length)
+  val wordsOverFive = words.filter(_.length > 5)
+
+  println(s"Word lengths: ${wordLengths.mkString(", ")}")
+  println(s"The long words: ${wordsOverFive.mkString(", ")}")
+
+}
