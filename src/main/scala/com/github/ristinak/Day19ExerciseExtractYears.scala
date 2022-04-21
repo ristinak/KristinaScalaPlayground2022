8fa6ff5f52856ff3cb99ff81c63008cad9b4cad2
ristinak <ristinak@gmail.com>
2022-04-21 21:59:33 +0300

    Fixed the year regex and added phone number regex.

--- src/main/scala/com/github/ristinak/Day19ExerciseExtractYears.scala
+++ src/main/scala/com/github/ristinak/Day19ExerciseExtractYears.scala
@@ -1,15 +1,19 @@
 package com.github.ristinak
 
 object Day19ExerciseExtractYears extends App {
-  //TODO extract all years into a Array(or list or vector) of Integers
+  //extract all years into a Array(or list or vector) of Integers
   //From the following src
   val src = "src/resources/ChristieA_Poirot_Investigates.txt" //you can choose another text if you wish
   //you can test your regex here first: https://regex101.com/ or somewhere else
 
   val text = Util.getTextFromFile(src)
 
-  val yearRegEx = raw"\d{4}".r
-  val years = yearRegEx.findAllIn(text).toArray.mkString(", ")
-  println(years)
+  val yearRegEx = raw"\b\d{4}\b".r
+  val years = yearRegEx.findAllIn(text).map(y => y.toInt).toArray
+  println(years.mkString(", "))
+
+  val phoneRegEx = raw"\(\d{3}\) \d{3}-\d{4}".r
+  val phoneNumbers = phoneRegEx.findAllIn(text).toArray
+  println(phoneNumbers.mkString(", "))
 
 }
